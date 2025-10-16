43cdd7ec368a416ac272542c97d7290021295ba7
Николай Аникин <n.anikin@2gis.ru>
2025-10-16 22:50:52 +0300

    mac support

--- setup/mac-setup.sh
+++ setup/mac-setup.sh
@@ -6,7 +6,7 @@ echo "Установка k8s-jprof"
 ZIP_URL="https://github.com/ZERDICORP/k8s-jprof/releases/download/1.0.beta/k8s-jprof-mac-release.zip"
 TMP_ZIP="/tmp/k8s-jprof-mac-release.zip"
 INSTALL_DIR="/usr/local/k8s-jprof"
-BIN_SYMLINK="/usr/local/bin/k8s-jprof"
+BIN_WRAPPER="/usr/local/bin/k8s-jprof"
 
 curl -L -o "$TMP_ZIP" "$ZIP_URL"
 
@@ -14,9 +14,14 @@ sudo mkdir -p "$INSTALL_DIR"
 sudo unzip -o "$TMP_ZIP" -d "$INSTALL_DIR"
 sudo chmod +x "$INSTALL_DIR/k8s-jprof"
 
-[ -L "$BIN_SYMLINK" ] && sudo rm "$BIN_SYMLINK"
-sudo ln -s "$INSTALL_DIR/k8s-jprof" "$BIN_SYMLINK"
+sudo tee "$BIN_WRAPPER" > /dev/null <<EOF
+#!/bin/bash
+"$INSTALL_DIR/k8s-jprof" "\$@"
+EOF
 
+sudo chmod +x "$BIN_WRAPPER"
+
+# Удаляем временный файл
 rm "$TMP_ZIP"
 
 echo "Установка завершена! Теперь можно запускать через терминал: k8s-jprof"
